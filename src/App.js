7d0403c91b225cec3868143b53f068a1aa2b2cd8
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2022-11-24 15:23:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {/* <Expenses items={expenses} /> */}
+      <Expenses items={expenses} />
     </div>
   );
 };
